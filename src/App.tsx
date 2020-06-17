1f2d48557e7d7d1e3e9cd4ddbde44360ca5000db
nullmage <jsky.johnson@gmail.com>
2020-06-16 23:21:14 -0600

    Adds some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello I am Sky
+        Hello I am Sky.
+
+        This site is a work in progress! Please check back soon!
       </header>
     </div>
   );
